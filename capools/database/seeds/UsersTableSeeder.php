6db332fbd70fb2d28703b9119373d9ccd739f50b
suupoo <github@suu-poo.com>
2020-11-17 23:01:04 +0900

    ロールのカラムを追加

--- capools/database/seeds/UsersTableSeeder.php
+++ capools/database/seeds/UsersTableSeeder.php
@@ -12,16 +12,25 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'  => 'テスト管理者',
+            'name'  => 'システム開発者',
             'email' => 'admin@example.com',
             'password' => '$2y$10$Sph1AvyUkRIeYQRC3Ve7AuDvyy.HTIrycnWaJtaZd.1siLYViWsni',
+            'role'  => 9
         ]);
 
         DB::table('users')->insert([
-            'name'  => 'テストユーザ1',
+            'name'  => '一般ユーザ(管理者)',
             'email' => 'user1@example.com',
             'password' => '$2y$10$jVC9esUe0VofJ0HUSkRQ0eDn7YMAnuTpqL5OW0h0yzJQbMSeS5QMe',
+            'role'  => 5
         ]);
+
+        DB::table('users')->insert([
+            'name'  => '一般ユーザ',
+            'email' => 'mentenance@example.com',
+            'password' => '$2y$10$jVC9esUe0VofJ0HUSkRQ0eDn7YMAnuTpqL5OW0h0yzJQbMSeS5QMe',
+        ]);
+
 
         // シーケンスの手動更新
         DB::unprepared("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
